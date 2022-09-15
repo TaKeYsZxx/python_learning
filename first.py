9650fdb3427ef311cbd6d96a7947432385af87e3
TaKeYsZxx <113452053+TaKeYsZxx@users.noreply.github.com>
2022-09-15 18:54:55 +0800

    Add files via upload
    学号：2020144147，姓名：张晓雪，第一次作业

---
+++ first.py
@@ -0,0 +1,13 @@
+import turtle as t
+t.seth(60)
+for i in range(3):
+  t.fd(150)
+  t.right(120)
+t.penup()
+t.fd(50)
+t.left(60)
+t.fd(50)
+t.pendown()
+for i in range(3):
+    t.right(120)
+    t.fd(150)
